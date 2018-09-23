cbe60b7235c34fc1d7a611dea4919b5ca4e18fdf
BorjaGomezSolorzano <BorjaGomezSolorzano@users.noreply.github.com>
2018-09-23 18:36:35 +0200

    static methods

--- deep-trader/rlFunctions.py
+++ deep-trader/rlFunctions.py
@@ -4,15 +4,30 @@ import tensorflow as tf
 class Functions:
 
     # ACTION
+    @classmethod
     def action(self, X, W_x, B_x):
+        """
+
+        :param X:
+        :param W_x:
+        :param B_x:
+        :return:
+        """
         return tf.tanh(tf.matmul(X, W_x) + B_x)
 
     # REWARD
+    @classmethod
     def reward(self, u, c, z_t, z_tm1):
         return u * z_t - c * tf.abs(z_t - z_tm1)
 
     # UTILITY function: Sharpe ratio
+    @classmethod
     def sharpe(self, returns):
+        """
+
+        :param returns:
+        :return:
+        """
         mu = 0
         sigma = 0
         size = len(returns)
@@ -26,5 +41,11 @@ class Functions:
 
         return mu / sigma
 
+    @classmethod
     def utility(self, returns):
+        """
+
+        :param returns:
+        :return:
+        """
         return self.sharpe(returns)
